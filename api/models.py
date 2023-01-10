4ba784f4ccc7f32a69d035da2f00a07caeb3cc39
yuutan1017 <n.you0611@gmail.com>
2023-01-10 23:04:12 +0900

    fix : model default image

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class About(models.Model):
-  image = models.ImageField(upload_to='images/about/', verbose_name='トップ画像')
+  image = models.ImageField(upload_to='images', verbose_name='トップ画像', blank=True, default='images/noImage.jpg')
   text = models.CharField(max_length=255)
 
   def __str__(self):
